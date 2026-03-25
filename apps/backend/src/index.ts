ba03674d011388e2467eb1eabb0362d5605a5215
tex <nathan.hostens@gmail.com>
2026-03-25 15:00:01 +0100

    edit listening configuration backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -29,7 +29,7 @@ fastify.get("/health", async () => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
     console.log("Server listening on http://localhost:3000");
   } catch (err) {
     fastify.log.error(err);
