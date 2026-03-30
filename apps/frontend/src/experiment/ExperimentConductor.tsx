28022160d9448b25a12152176dd4373f6032794d
Texleretour <72305552+Texleretour@users.noreply.github.com>
2026-03-30 13:18:12 +0200

    Update apps/frontend/src/experiment/ExperimentConductor.tsx

--- apps/frontend/src/experiment/ExperimentConductor.tsx
+++ apps/frontend/src/experiment/ExperimentConductor.tsx
@@ -33,7 +33,7 @@ export default function ExperimentConductor({ participantCode }: ExperimentCondu
     | "inmExplanation"
     | "inm"
     | "finished"
-  >("learning");
+  >("learningExplanationsBeforeDemo");
 
   const learningResponseKeys = useRef<LearningResponseKeys>({
     trueKey: "l", // default value
